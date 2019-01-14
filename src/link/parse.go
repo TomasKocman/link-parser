82f2cb43a4c3ef8a306d1ff3493551c03d38b7f8
Tomas Kocman <t.kocmancz@gmail.com>
2019-01-14 17:31:42 +0100

    .

--- src/link/parse.go
+++ src/link/parse.go
@@ -1,8 +1,8 @@
 package link
 
 import (
-	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -18,12 +18,43 @@ func Parse(r io.Reader) ([]Link, error) {
 		return nil, err
 	}
 
+	var links []Link
 	nodes := linkNodes(doc)
 	for _, node := range nodes {
-		fmt.Println(node)
+		links = append(links, buildLink(node))
 	}
 
-	return nil, nil
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+
+	ret.Text = text(n)
+
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+
+	return strings.Join(strings.Fields(ret), " ")
 }
 
 func linkNodes(n *html.Node) []*html.Node {
