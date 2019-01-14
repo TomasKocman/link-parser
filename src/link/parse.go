c5748035794c7b159bef303b963f4270c4344345
Tomas Kocman <t.kocmancz@gmail.com>
2019-01-14 16:23:04 +0100

    .

---
+++ src/link/parse.go
@@ -0,0 +1,40 @@
+package link
+
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	nodes := linkNodes(doc)
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
+
+	return nil, nil
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+
+	return ret
+}
